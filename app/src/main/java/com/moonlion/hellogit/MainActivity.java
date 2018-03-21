5ac912d4032d84a55c77495d05fe60f28dc84af1
wzh <913447093@qq.com>
2018-03-21 14:00:31 +0800

    在分支删除二次修改

--- app/src/main/java/com/moonlion/hellogit/MainActivity.java
+++ app/src/main/java/com/moonlion/hellogit/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("首次提交");
         //制造冲突
-        System.out.println("二次提交,对二次提交进行修改");
         System.out.println("在另一个项目提交");
     }
 }
