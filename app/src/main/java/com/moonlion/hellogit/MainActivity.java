896cd3ee1393a57d65d87080fc0720159fad880d
wzh <913447093@qq.com>
2018-03-21 13:56:36 +0800

    制造冲突

--- app/src/main/java/com/moonlion/hellogit/MainActivity.java
+++ app/src/main/java/com/moonlion/hellogit/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("首次提交");
+        //制造冲突
         System.out.println("二次提交,对二次提交进行修改");
         System.out.println("在另一个项目提交");
     }
